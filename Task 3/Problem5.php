42a81e8b6bf685e398bfa04a060967d0d75025ba
Saifullah <saifullah.gazi2000@gmail.com>
2021-06-13 13:24:39 +0600

    Problem 5 solved properly

--- Task 3/Problem5.php
+++ Task 3/Problem5.php
@@ -1,20 +1,22 @@
 <html>
     <head>
-        <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Lab3_task(Problem 5)</title>
     </head>
     <body>
         <?php
-     $backColor="green";
-     $fontColor="red";
-     $imageWidth="80px";
-     $imageHeight="80px";
+     $backColor="cyan";
+     $fontColor="black";
+     $imageWidth="150px";
+     $imageHeight="150px";
     ?>
-    <img src="Aiub_Logo.jpg" width="<?php echo $imageWidth; ?>" height="<?php echo $imageHeight; ?>" />    
-    <h2>American International University -<span style="background-color:<?php echo $backColor; ?> color:<?echo $fontColor; ?>"> Bangladesh
-    </h2>
+    <table>
+    
+    <tr>
+    <td><img src="Aiub_Logo.jpg" width="<?php echo $imageWidth; ?>" height="<?php echo $imageHeight; ?>" /> </td>
 
+    <td><h2><span style="background-color:<?php echo $backColor; ?>;color:<?php echo $fontColor; ?>">American International University - Bangladesh</h2>
+</span></td>
+    </tr>
+    </table>
     </body>
 </html>
